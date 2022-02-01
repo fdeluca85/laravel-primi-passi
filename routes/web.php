c1b6f680dbc84c85b841309de1e0be6c20028e18
francesca <fradeluca85@gmail.com>
2022-02-01 15:31:22 +0100

    hello world

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Hello world</h1>";
+});
+
+Route::get('/chi-siamo', function () {
+    return "<h2>Pagina chi</h2>";
 });
